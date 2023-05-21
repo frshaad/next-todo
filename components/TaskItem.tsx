57f27356cd840328a3ccc82edd5d1bc447e450c4
Farshad <farshad.hatami95@gmail.com>
2023-05-21 08:32:52 +0330

    feat: delete task

--- components/TaskItem.tsx
+++ components/TaskItem.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
-import { BsStar, BsStarFill } from 'react-icons/bs';
+import { BsStar, BsStarFill, BsTrash3, BsPencilSquare } from 'react-icons/bs';
 
 import useTasks from '@/hooks/useTasks';
+import Modal from './Modal';
 
 const TaskItem = ({ id, isTaskDone, title, isImportant }: Task) => {
-  const { toggleTaskDone, toggleImportance } = useTasks();
+  const { toggleTaskDone, toggleImportance, deleteTask } = useTasks();
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [starOpacity, setStarOpacity] = useState<'hidden' | 'block'>('hidden');
 
   const toggleExpandedTaskCard = () => {};
+
+  const modalDeleteMessage = 'Do you really want to remove this task?';
 
   return (
     <div
@@ -18,6 +22,13 @@ const TaskItem = ({ id, isTaskDone, title, isImportant }: Task) => {
       onMouseOut={() => setStarOpacity('hidden')}
       onMouseOver={() => setStarOpacity('block')}
     >
+      {isModalOpen && (
+        <Modal
+          message={modalDeleteMessage}
+          confirmFn={() => deleteTask(id)}
+          setIsModalOpen={setIsModalOpen}
+        />
+      )}
       <div className='flex items-center gap-3'>
         <input
           type='checkbox'
@@ -38,7 +49,13 @@ const TaskItem = ({ id, isTaskDone, title, isImportant }: Task) => {
           </h3>
         </div>
       </div>
-      <div>
+      <div className='flex items-center justify-end gap-4'>
+        <div
+          className={`${starOpacity} transition duration-200 hover:text-red-600 dark:hover:text-red-300`}
+          onClick={() => setIsModalOpen(true)}
+        >
+          <BsTrash3 size={20} opacity={0.5} />
+        </div>
         <div
           className={`transition duration-200 ${
             isImportant ? '' : starOpacity
